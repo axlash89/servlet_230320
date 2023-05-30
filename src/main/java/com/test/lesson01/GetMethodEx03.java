4062b32a6e882f9c7ba8b2970f7269bd280ecf54
test <test@test.com>
2023-05-30 18:20:05 +0900

    테스트 주석 추가

--- src/main/java/com/test/lesson01/GetMethodEx03.java
+++ src/main/java/com/test/lesson01/GetMethodEx03.java
@@ -14,6 +14,8 @@ public class GetMethodEx03 extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
+		// test code
+		
 		response.setContentType("text/json");
 		response.setCharacterEncoding("utf-8");
 //		response.setContentType("text/plain");
